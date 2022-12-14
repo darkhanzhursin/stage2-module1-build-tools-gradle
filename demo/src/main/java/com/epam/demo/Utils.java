148da2b4245c7725a0fe5f1c2dba182f57a8327d
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-12-14 13:57:25 +0600

    Task #1. Build Tools. Gradle

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,18 @@
 package com.epam.demo;
 
+import java.util.Iterator;
 import java.util.List;
+
+import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        Iterator<String> iterator = args.iterator();
+        while (iterator.hasNext()) {
+            if (StringUtils.isPositiveNumber(iterator.next()) == false) {
+               return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,15 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
+        int res = NumberUtils.createInteger(str);
+        if (res > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
