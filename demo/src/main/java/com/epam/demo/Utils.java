cd2f875c2bd9dd0c474e7d9e50ee55d2d422bc45
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-12-14 14:06:57 +0600

    Task #1. fixed list

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -7,12 +7,14 @@ import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        Iterator<String> iterator = args.iterator();
-        while (iterator.hasNext()) {
-            if (StringUtils.isPositiveNumber(iterator.next()) == false) {
-               return false;
+        if (!args.isEmpty() || args.size() > 0) {
+            for (int i = 0; i < args.size(); i++) {
+                if (StringUtils.isPositiveNumber(args.get(i)) == false) {
+                    return false;
+                }
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
