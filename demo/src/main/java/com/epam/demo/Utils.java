cfccdd9386444b11a3a9707a52a5e0cce2169450
Darkhan Zhursin <daratechn@gmail.com>
2024-03-28 14:43:01 -0500

    refactoring

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,6 +1,5 @@
 package com.epam.demo;
 
-import java.util.Iterator;
 import java.util.List;
 
 import com.epam.utils.StringUtils;
@@ -8,10 +7,8 @@ import com.epam.utils.StringUtils;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         if (!args.isEmpty() || args.size() > 0) {
-            for (int i = 0; i < args.size(); i++) {
-                if (StringUtils.isPositiveNumber(args.get(i)) == false) {
-                    return false;
-                }
+            for (String str : args) {
+                return StringUtils.isPositiveNumber(str);
             }
             return true;
         }
